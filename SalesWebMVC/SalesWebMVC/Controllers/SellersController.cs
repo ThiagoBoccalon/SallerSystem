981d1d2055907101a2b6e60e27866b6c0cc76cef
ThiagoBoccalon <thiago_rodrigo90@hotmail.com>
2022-04-02 21:55:17 +0100

    Simple Create form

--- SalesWebMVC/SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/SalesWebMVC/Controllers/SellersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMVC.Data;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Controllers
 {
@@ -18,6 +19,28 @@ namespace SalesWebMVC.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Seller.ToListAsync());
+        }
+
+        // GET: Sellers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sellers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Email,BirthDate,BaseSalary,Department")] Seller seller)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(seller);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(seller);
         }
     }
 }
