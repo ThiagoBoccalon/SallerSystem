3f3fe27c99640ef77df850443ebed44a29d5b130
ThiagoBoccalon <thiago_rodrigo90@hotmail.com>
2022-04-21 10:05:15 +0100

    Validation to the client.

--- SalesWebMVC/SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/SalesWebMVC/Models/Seller.cs
@@ -9,18 +9,29 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} requied")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} amd {1}")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} requied")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} requied")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+
+        [Required(ErrorMessage = "{0} requied")]
+        [Range(50.0, 1000000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
